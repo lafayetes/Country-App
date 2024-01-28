3e3be233013820a259a826b5a6d04484e50f340e
Diego La Faye <lafayediego@gmail.com.com>
2024-01-28 12:35:55 -0400

    added optimizations to the by region page

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,19 +2,23 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
 @Component({
   selector: 'countries-by-region-page',
   templateUrl: './by-region-page.component.html',
   styles: ``
 })
 export class ByRegionPageComponent {
-  constructor(private countriesService:CountriesService){}
+  constructor(private countriesService: CountriesService) { }
   public countries: Country[] = [];
-  searchByRegion(term:string){
-    this.countriesService.searchByRegion(term)
-    .subscribe(countries =>{
-      this.countries = countries;
-    });
+  public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania']
+  public selectedRegion?: Region;
+  searchByRegion(region: Region) {
+    this.selectedRegion = region;
+    this.countriesService.searchByRegion(region)
+      .subscribe(countries => {
+        this.countries = countries;
+      });
 
   }
 }
